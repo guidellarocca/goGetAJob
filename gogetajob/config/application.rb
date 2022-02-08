726d1a0cbc8ceec60db043996e4d500e02993c0a
Riccardo Di Biasi <dibiasi.1694305@studenti.uniroma1.it>
2022-02-08 02:19:16 +0100

    added some gems + devise installed + added user table + changed some views and style

--- gogetajob/config/application.rb
+++ gogetajob/config/application.rb
@@ -9,6 +9,10 @@ Bundler.require(*Rails.groups)
 module Gogetajob
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    
+    config.application_name = "GoGetAJob"
+    
+    
     config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
--- gogetajob/config/routes.rb
+++ gogetajob/config/routes.rb
@@ -1,6 +1,13 @@
+require "sidekiq/web"
+
 Rails.application.routes.draw do
+  devise_for :users
+
+  devise_scope :user do
+    get "users/sign_out" => "devise/sessions#destroy"
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+   root "home#index"
 end
