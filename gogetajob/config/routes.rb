cec9c95f8ad9c44e6d00b3e842c40a08c196e8b2
guidellarocca <45170989+guidellarocca@users.noreply.github.com>
2022-02-08 23:35:13 +0100

    added jobs tables with scaffold + dependencies users-jobs + now the uses can post a new job + views + some styles

--- gogetajob/config/routes.rb
+++ gogetajob/config/routes.rb
@@ -1,6 +1,7 @@
 require "sidekiq/web"
 
 Rails.application.routes.draw do
+  resources :jobs
   devise_for :users
 
   devise_scope :user do
@@ -9,5 +10,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-   root "home#index"
+   root "jobs#index"
 end
--- gogetajob/db/schema.rb
+++ gogetajob/db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_08_002221) do
+ActiveRecord::Schema.define(version: 2022_02_08_221008) do
+
+  create_table "jobs", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "url"
+    t.string "job_type"
+    t.string "location"
+    t.string "job_author"
+    t.boolean "remote_ok"
+    t.string "apply_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "avatar"
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
