254e5fb02084d9c3e11ccb437d2bf5f6c0b1d121
Riccardo Di Biasi <dibiasi.1694305@studenti.uniroma1.it>
2022-02-08 02:26:04 +0100

    rails db:migrate executed

--- gogetajob/db/schema.rb
+++ gogetajob/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_07_223813) do
+ActiveRecord::Schema.define(version: 2022_02_08_002221) do
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 2022_02_07_223813) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin"
+    t.string "stripe_id"
+    t.string "card_brand"
+    t.string "card_last4"
+    t.string "card_exp_month"
+    t.string "card_exp_year"
+    t.datetime "expires_at", precision: 6
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
